3d2e87dcb0d33afbb5093ed4cf09748fa192a727
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-04-28 14:15:32 +0200

    Simple C Programm

---
+++ C/reverse & count.c
@@ -0,0 +1,56 @@
+/*
+ * #include<stdio.h>
+
+int main(){
+		for(int i = 10; i>=1; i--)
+			printf("%d\n",i);
+		return 0;
+}
+
+
+// Sum of The first 20 natural numbers
+#include<stdio.h>
+
+int main(){
+	int sum = 0;
+	for(int i = 1; i<=20; i++)
+		sum +=i;
+		
+	printf("the sum is as follows %d", sum);
+	return 0;
+}
+
+
+
+// Check if a number is positive, negative or zero
+#include<stdio.h>
+
+int main(){
+	int num;
+	printf("enter number: ");
+	scanf("%d", &num);
+	if(num > 0)
+		printf("the number is positive");
+	else if(num < 0)
+		printf("the number is negative");
+	else
+		printf("the number is zero");
+	return 0;
+}
+*/
+
+
+
+//swap sides with strings/or more like a reverse
+
+#include<stdio.h>
+
+void swap(char s[], int i, int j){
+	char tmp = s[i];
+	s[i] = s[j];
+	s[j] = tmp;
+}
+
+void twaje
+
+
